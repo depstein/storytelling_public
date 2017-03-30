11e6bd8430b565c2289ea68af1596a82a92302ea
Daniel Epstein <depstein@cs.washington.edu>
2017-03-30 15:54:49 -0700

    Amazingly, social features were super easy to implement.

--- src/pages/review-chapter/review-chapter.ts
+++ src/pages/review-chapter/review-chapter.ts
@@ -1,8 +1,9 @@
 import { Component, Input } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, Platform } from 'ionic-angular';
 import { ChapterData } from '../../models/chapter-data';
 import { ViewChapterComponent } from '../../components/view-chapter/view-chapter';
 import { DataStorage } from '../../providers/data-storage';
+import { SocialSharing } from '@ionic-native/social-sharing';
 
 /*
   Generated class for the ReviewChapter page.
@@ -13,17 +14,28 @@ import { DataStorage } from '../../providers/data-storage';
 @Component({
   selector: 'page-review-chapter',
   templateUrl: 'review-chapter.html',
-  providers: [ DataStorage ]
+  providers: [ DataStorage, SocialSharing ]
 })
 export class ReviewChapterPage {
   @Input('view-chapter') viewChapter: ViewChapterComponent;
   chapterData:ChapterData;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private dataStore:DataStorage) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private platform : Platform, private dataStore:DataStorage, private social:SocialSharing) {
     this.chapterData = this.navParams.get("chapterData");
   }
 
   ionViewDidLoad() {
+  }
+
+  shareViaFacebook() {
+    if(this.platform.is('cordova')) {
+      this.social.share("<html><body>THIS IS THE <strong>best!</strong>...<br>AGAIN, with feeling!</body></html>", "", this.chapterData.run.staticMapUrl).then(() => {
+        console.log('Shared correctly!');
+      }).catch((reason) => {
+        console.log('Something bad happened');
+        console.log(reason);
+      });
+    }
   }
 
   completeChapter() {
